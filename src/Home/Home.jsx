b72ad9bd0670b6290c0609722fd8218f9a0dbc38
unknown <mebannongrum333@gmail.com>
2025-06-28 12:10:29 +0530

    build home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import NavBar from '../components/Navbar/Navbar'
+import HeroSlider from '../components/HeroSlider/HeroSlider'
+import SearchHospital from '../components/SearchHospital/SearchHospital'
+import HeroServices from '../components/IconLayout/HeroServices'
+import Offers from '../components/Sections/Offers/Offers'
+import Specialization from '../components/Sections/Specialization/Specialization'
+import Specialists from '../components/Sections/Specialists/Specialists'
+import PatientCaring from '../components/Sections/PatientCaring/PatientCaring'
+import Blogs from '../components/Sections/Blogs/Blogs'
+import OurFamilies from '../components/Sections/OurFamilies/OurFamilies'
+import FAQs from '../components/Sections/FAQs/FAQs'
+import DownloadApp from '../components/Sections/DownloadApp/DownloadApp'
+import Footer from '../components/Footer/Footer'
+
+
 function Home() {
   return (
     <div>
       <NavBar />
-      {/* Hello */}
+      <HeroSlider />
+      <SearchHospital />
+      <HeroServices />
+      <Offers />
+      <Specialization />
+      <Specialists />
+      <PatientCaring />
+      <Blogs />
+      <OurFamilies />
+      <FAQs />
+      <DownloadApp />
+      <Footer />
     </div>
   )
 }
